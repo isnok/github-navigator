6a917708fa774f92a5a8758428e658c09e43eac3
Konstantin Martini <k@martini.pm>
2017-06-19 13:37:57 +0200

    sort commits in python + date formatting

--- application.py
+++ application.py
@@ -23,12 +23,12 @@ app.config.from_object('application.NavigatorConfig')
 
 api = Api(app)
 
-def sort_data(items, **kwargs):
+def sort_repos(items, **kwargs):
 
     if 'key' not in kwargs:
-        def sort__created_at__iso8601(item):
+        def sort_key__created_at__iso8601(item):
             return dateutil.parser.parse(item['created_at'])
-        kwargs['key'] = sort__created_at__iso8601
+        kwargs['key'] = sort_key__created_at__iso8601
 
     # default to descending sort
     if 'reverse' not in kwargs:
@@ -40,6 +40,12 @@ def sort_data(items, **kwargs):
     # sort_key = lambda item: datetime.datetime.strptime(item['created_at'], "%Y-%m-%dT%H:%M:%SZ")
     # return sorted(items, key=sort_key)
 
+def sort_commits(items, **kwargs):
+    """ just to make sure """
+    def sort_key__commiter_date__iso8601(item):
+        return dateutil.parser.parse(item['commit']['committer']['date'])
+    return sorted(items, key=sort_key__commiter_date__iso8601, reverse=True)
+
 term_parser = reqparse.RequestParser()
 term_parser.add_argument('search_term', type=str, default='')
 
@@ -49,7 +55,7 @@ class NavigatorSearchResource(Resource):
         search_term = term_parser.parse_args()['search_term']
         search_result = search_repositories(search_term)
         if search_result['github_status'] == 200:
-            return sort_data(search_result['github_data']['items'])
+            return sort_repos(search_result['github_data']['items'])
         else:
             return None
 
@@ -70,35 +76,41 @@ def search_repos():
     )
 
     if search_result['github_status'] == 200:
-        repos = sort_data(search_result['github_data']['items'])
+        repos = sort_repos(search_result['github_data']['items'])
     else:
         repos = []
 
     repos = repos[:MAX_RESULTS]
 
     commit_data = {}
-    no_commit_data = {}
+    extra_data = {}
     for search_result in repos:
+        repo_extra = {
+            'datetime_created': dateutil.parser.parse(search_result['created_at']),
+        }
         result = repository_details(
             search_result,
             session=session,
             oauth=OAUTH_CREDENTIALS,
         )
         if result['github_status'] == 200:
-            commit_data[search_result['id']] = result['github_data']
+            commit_data[search_result['id']] = sort_commits(result['github_data'])
+            latest_commit_url = result['github_data'][0]['html_url']
+            repo_extra['latest_commit_url'] = latest_commit_url
         else:
             commit_data[search_result['id']] = None
             fail_data = result['github_data']
             fail_data['check_url'] = search_result['commits_url'].replace('{/sha}', '')
             fail_data.setdefault('message', None)
-            no_commit_data[search_result['id']] = fail_data
+            repo_extra.update(fail_data)
+        extra_data[search_result['id']] = repo_extra
 
     return render_template(
         'results.html',
         search_term=search_term,
         items=repos,
         commit_data=commit_data,
-        no_commit_data=no_commit_data,
+        extra_data=extra_data,
     )
 
 
@@ -117,6 +129,10 @@ def load_oauth_file(name):
         app.logger.info("Failed to load OAUTH credentials from '{file}'.\nException was: {exc}".format(file=name, exc=ex))
 
 load_oauth_file('oauth.json')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
